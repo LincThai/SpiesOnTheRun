e9e8fdd91b0faf46767bf805af8c74a10f006c6b
LincThai <lincoln.thai.gamedesign@gmail.com>
2021-06-16 11:20:31 +1000

    New Obstacle, Game Manager & new tags

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameState gameState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameState = GameState.preGame;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public enum GameState { preGame, game, dead};
